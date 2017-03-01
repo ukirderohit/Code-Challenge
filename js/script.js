6860a41a9e981a95982c53a4031fb01ebb2e34d7
Rohit Ukirde <ukirderohit@gmail.com>
2017-02-28 23:30:17 -0600

    added js for animate.css

--- js/script.js
+++ js/script.js
@@ -9,3 +9,31 @@ $(window).scroll(function() {
     mn.removeClass(mns);
   }
 });
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+    $('.icondown').each(function() {
+        animationHover(this, 'bounce');
+    });
+
+    /*hover*/
+function animationHover(element, animation){
+    element = $(element);
+    element.hover(
+        function() {
+            element.addClass('animated ' + animation);        
+        },
+        function(){
+            //wait for animation to finish before removing classes
+            window.setTimeout( function(){
+                element.removeClass('animated ' + animation);
+            }, 1000);         
+        });
+}
+});
